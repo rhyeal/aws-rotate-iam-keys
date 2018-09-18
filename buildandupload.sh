1aa16ccaeb363bb87552b07310fac011cfb5e86f
Adam Link <rhyeal@gmail.com>
2018-09-18 14:43:55 -0800

    new build script for PPA

--- buildandupload.sh
+++ buildandupload.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-VERSION=$1
+VERSION="aws-rotate-iam-keys_$1_source.changes"
 
 cd src
 dch -i
 
 debuild -S -sa
 cd ..
-dput ppa:rhyeal/aws-rotate-iam-keys aws-rotate-iam-keys_$VERSION_source.changes
+dput ppa:rhyeal/aws-rotate-iam-keys $VERSION
