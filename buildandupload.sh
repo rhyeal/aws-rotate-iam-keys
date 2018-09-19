d4f3fcbb6fd06576a54c2534e98a0c767ee6685b
Adam Link <rhyeal@gmail.com>
2018-09-18 17:17:44 -0800

    zips and working build!

--- buildandupload.sh
+++ buildandupload.sh
@@ -2,8 +2,8 @@
 VERSION="aws-rotate-iam-keys_$1_source.changes"
 
 cd src
-dch -i --distribution bionic --force-distribution
+DEBEMAIL="Adam Link <aws-rotate-iam-keys@rhyeal.com>" DEBFULLNAME="Adam Link" dch -i --distribution bionic --force-distribution
 
-debuild -S -sa
+DEBEMAIL="Adam Link <aws-rotate-iam-keys@rhyeal.com>" DEBFULLNAME="Adam Link" debuild -S -sa
 cd ..
 dput ppa:rhyeal/aws-rotate-iam-keys $VERSION
