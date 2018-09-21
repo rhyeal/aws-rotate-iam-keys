c4808093deaea7894499f399b985896ee6b48c08
Adam Link <rhyeal@gmail.com>
2018-09-20 17:46:19 -0800

    now with zip

--- buildandupload.sh
+++ buildandupload.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 VERSION="aws-rotate-iam-keys_$1_source.changes"
 
+# make the homebrew zip file
+zip -r aws-rotate-iam-keys_$VERSION src
+
 cd src
 DEBEMAIL="Adam Link <aws-rotate-iam-keys@rhyeal.com>" DEBFULLNAME="Adam Link" dch -i --distribution bionic --force-distribution
+
+
 
 DEBEMAIL="Adam Link <aws-rotate-iam-keys@rhyeal.com>" DEBFULLNAME="Adam Link" debuild -S -sa
 cd ..
 dput ppa:rhyeal/aws-rotate-iam-keys $VERSION
-
-# make the homebrew zip file
-zip -r aws-rotate-iam-keys_$VERSION src
