af68bb95ed99d6f2b992adab171dff434dd96486
Adam Link <rhyeal@gmail.com>
2018-10-01 16:13:55 -0800

    easy install windows now!

--- Windows/aws-rotate-iam-keys.ps1
+++ Windows/aws-rotate-iam-keys.ps1
@@ -5,6 +5,18 @@ param (
 
 $arr = $profiles -split ','
 
+# Schedule the cronjob
+If (Get-ScheduledTask | Where-Object {$_.TaskName -like "AWS Rotate IAM Keys" }) {
+    Write-Host "Cronjob already installed."
+} Else {
+    $folder = Split-Path $MyInvocation.MyCommand.Path -Parent
+    $hour = Get-Random -Maximum 6 -Minimum 2
+    $minute = Get-Random -Maximum 60
+
+    $createTask = "schtasks /create /f /tn `"AWS Rotate IAM Keys`" /tr `"Powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -nologo -noninteractive '$folder\aws-rotate-iam-keys.ps1' -profile default`" /sc daily /st 0$($hour):$minute"
+    Invoke-Expression $createTask
+}
+
 <#
 
 Returns true if a program with the specified display name is installed.
@@ -34,12 +46,12 @@ function Load-Module ($m) {
     } else {
         # If module is not imported, but available on disk then import
         if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $m}) {
-            Import-Module $m -Verbose
+            Import-Module $m
         } else {
             # If module is not imported, not available on disk, but is in online gallery then install and import
             if (Find-Module -Name $m | Where-Object {$_.Name -eq $m}) {
-                Install-Module -Name $m -Force -Verbose -Scope CurrentUser
-                Import-Module $m -Verbose
+                Install-Module -Name $m -Force -Scope CurrentUser
+                Import-Module $m
             } else {
                 # If module is not imported, not available and not in online gallery then abort
                 write-host "Module $m not imported, not available and not in online gallery, exiting."
@@ -48,6 +60,7 @@ function Load-Module ($m) {
         }
     }
 }
+
 
 If (Is-Installed("AWS Command Line Interface")) {
     Write-Host "AWS CLI installed."
@@ -68,15 +81,26 @@ AppData\Local\AWSToolkit\RegisteredAccounts.json
 #>
 
 # Set the profile used and default to default
-Set-AWSCredential -ProfileName $arr[0]
+Try {
+    Set-AWSCredential -ProfileName $arr[0]
+} Catch {
+    Write-Host "Could not load profile. Please set up AWS PowerShell Tools first before running this command!"
+    Exit $LASTEXITCODE
+}
 
 $current_key = Get-AWSCredential -ProfileName $arr[0]
 
 $location = "C:\Users\$($env:UserName)\.aws\credentials"
 
 #Create a new AWS Key
-$new_key = New-IAMAccessKey -ProfileName $arr[0]
-Write-Output "Making new access key"
+Try {
+    Write-Output "Making new access key"
+    $new_key = New-IAMAccessKey -ProfileName $arr[0]
+} Catch {
+    Write-Host "Could not make access key. Do you already have 2 keys?"
+    Exit $LASTEXITCODE
+}
+
 
 #Delete the AWS Key
 Remove-IAMAccessKey -Force -AccessKeyId $current_key.GetCredentials().AccessKey -ProfileName $arr[0] -Confirm:$false
