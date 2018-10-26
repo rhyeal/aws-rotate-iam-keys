349853007be93264dd4d7194497e363b239edc7d
Adam Link <rhyeal@gmail.com>
2018-10-26 11:45:49 -0800

    homebrew sha bump

--- Formula/aws-rotate-iam-keys.rb
+++ Formula/aws-rotate-iam-keys.rb
@@ -1,8 +1,8 @@
 class AwsRotateIamKeys < Formula
   desc "Automatically set up a cron job to rotate your IAM keys"
   homepage "https://aws-rotate-iam-keys.com"
-  url "https://github.com/rhyeal/aws-rotate-iam-keys/archive/v0.6.0.tar.gz"
-  sha256 "d702b7c5f43f1935d8a824adfbc36329f4aa95892e6eda1b102e38ac772970be"
+  url "https://github.com/rhyeal/aws-rotate-iam-keys/archive/v0.7.0.tar.gz"
+  sha256 "74929d72ff1b694654f49a65046e81ce229a63deeee378cb54f5a57c53b90ad9"
   depends_on "awscli"
   depends_on "jq"
 
