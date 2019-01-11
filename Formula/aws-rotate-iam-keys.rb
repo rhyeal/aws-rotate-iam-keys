d714d044ac4399ff3b91cc3d126633404aec939e
Mark Woods <mwoods.online.ie@gmail.com>
2019-01-11 08:30:14 +0000

    brew audit fix - gnu-getopt should be before jq

--- Formula/aws-rotate-iam-keys.rb
+++ Formula/aws-rotate-iam-keys.rb
@@ -4,8 +4,8 @@ class AwsRotateIamKeys < Formula
   url "https://github.com/rhyeal/aws-rotate-iam-keys/archive/v0.8.1.tar.gz"
   sha256 "bff7a999f402db12114fae91d46455e5f36b9559fd4a07caad09c5f42a99b8d6"
   depends_on "awscli"
-  depends_on "jq"
   depends_on "gnu-getopt"
+  depends_on "jq"
 
   def install
     bin.install "src/bin/aws-rotate-iam-keys"
