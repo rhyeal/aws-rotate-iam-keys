6aa544ac50f329391f071fa4dd442605e5e994ca
Mark Woods <mwoods.online.ie@gmail.com>
2023-06-22 07:51:01 +0100

    Remove Homebrew formula template, update manually
    Copy recent changes to Homebrew formula template by @Volatus to actual
    formula file and delete the template. CircleCI builds are broken, and
    using a template for this was probably a bad idea anyway.
    
    This does not update the latest stable release, it remains at 0.9.8.4
    
    To install the latest version, use `brew install --HEAD`

--- Formula/aws-rotate-iam-keys.rb
+++ Formula/aws-rotate-iam-keys.rb
@@ -1,21 +1,14 @@
-=begin
-This is an auto-generated file. Please make changes to the file with .template. in the name.
-Hint: Look in the project root folder for the template file
-=end
-
 class AwsRotateIamKeys < Formula
   desc "Automatically rotate your IAM keys daily"
   homepage "https://aws-rotate-iam-keys.com"
   url "https://github.com/rhyeal/aws-rotate-iam-keys/archive/v0.9.8.4.tar.gz"
   sha256 "47912ee7c1adb93ac602d54e6689d2609c00166de4a46cd00f2a42bf37d6921b"
-  depends_on "awscli" => :recommended
   depends_on "gnu-getopt"
   depends_on "jq"
+  depends_on "awscli" => :recommended
 
   head do
-    Dir.chdir(File.expand_path(File.join(File.dirname(__FILE__), '../'))) do
-      url %x{git config --local --get remote.origin.url | tr -d '\n'}, using: :git
-    end
+    url "https://github.com/rhyeal/aws-rotate-iam-keys.git"
   end
 
   def install
@@ -27,7 +20,7 @@ class AwsRotateIamKeys < Formula
   end
 
   def caveats
-    s = <<~EOS
+    <<~EOS
       We've installed a default/global configuration file to:
           #{etc}/aws-rotate-iam-keys
 
@@ -50,42 +43,17 @@ class AwsRotateIamKeys < Formula
     EOS
   end
 
-  plist_options :startup => false
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-      <key>EnvironmentVariables</key>
-      <dict>
-        <key>PATH</key>
-        <string>#{bin}:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-      </dict>
-      <key>Label</key>
-      <string>#{plist_name}</string>
-      <key>ProgramArguments</key>
-      <array>
-        <string>/bin/bash</string>
-        <string>-c</string>
-        <string>if ! curl -s www.google.com > /dev/null; then sleep 60; fi; cp /dev/null /tmp/#{plist_name}.log ; ( egrep '^[[:space:]]*-' ~/.aws-rotate-iam-keys 2>/dev/null || cat #{etc}/aws-rotate-iam-keys ) | while read line; do aws-rotate-iam-keys $line; done</string>
-      </array>
-      <key>StandardOutPath</key>
-      <string>/tmp/#{plist_name}.log</string>
-      <key>StandardErrorPath</key>
-      <string>/tmp/#{plist_name}.log</string>
-      <key>RunAtLoad</key>
-      <true/>
-      <key>StartCalendarInterval</key>
-      <dict>
-        <key>Hour</key>
-        <integer>3</integer>
-        <key>Minute</key>
-        <integer>23</integer>
-      </dict>
-    </dict>
-    </plist>
-  EOS
+  def log_path
+    "/tmp/#{plist_name}.log"
+  end
+  service do
+    run ["bash", "-c", "if ! curl -s www.google.com; then sleep 60; fi; cp /dev/null #{f.log_path} ; ( grep -E ^[[:space:]]*- ~/.aws-rotate-iam-keys || cat #{etc}/aws-rotate-iam-keys ) | while read line; do #{opt_bin}/aws-rotate-iam-keys $line; done"]
+    run_type :cron
+    run_at_load true
+    cron "23 3 * * *"
+    environment_variables PATH: std_service_path_env
+    log_path f.log_path
+    error_log_path f.log_path
   end
 
   test do
